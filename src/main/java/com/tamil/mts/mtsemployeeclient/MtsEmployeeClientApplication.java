f3552d9b3b7bc25855819acd4b69653402488924
Murugan Nagarajan <murugan425@gmail.com>
2020-10-06 00:20:28 +0530

    Created a client application using spring RestTemplate for the employee management micro-service.

---
+++ src/main/java/com/tamil/mts/mtsemployeeclient/MtsEmployeeClientApplication.java
@@ -0,0 +1,13 @@
+package com.tamil.mts.mtsemployeeclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsEmployeeClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtsEmployeeClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamil/mts/mtsemployeeclient/MtsEmployeeClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamil.mts.mtsemployeeclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtsEmployeeClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
